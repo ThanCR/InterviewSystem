e28fb762a64a15d1e2e950fa6ce93d83f0ac757d
Jonnathan <jonzxz12@gmail.com>
2023-06-09 05:27:00 -0600

    Client - Progress on client-side consuming services

--- InterviewSystem.Client/Program.cs
+++ InterviewSystem.Client/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using InterviewSystem.ET.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+const string APIurl = "https://localhost:7128/ProductDescription";
+
+//Creating new http client
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        HttpResponseMessage response = await client.GetAsync(APIurl);
+
+        if (response.IsSuccessStatusCode)
+        {
+            APIResponse<List<ProductDescription>> jsonContent 
+                = await response.Content.ReadFromJsonAsync<APIResponse<List<ProductDescription>>>();
+
+            List<ProductDescription> result = jsonContent.Response;
+
+            result.ForEach(x =>
+            {
+                Console.WriteLine($"" +
+                    $"Name: {x.ProductName}\n" +
+                    $"Aspect: {x.ProductDescriptionTypeName}\n" +
+                    $"Description: {x.ProductDescriptionName} \n");
+            });
+        }
+        else
+        {
+            Console.WriteLine("An error ocurred when processing request");
+        }
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+}
+
+Console.ReadLine();
